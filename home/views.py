f7e992a8335df22fa9f039a041ee1650ecd6be00
Saurav <sp.gharti@gmail.com>
2018-03-14 18:59:06 +0000

    add package adminlettuce

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'adminlettuce', 'url': 'http://pypi.python.org/pypi/adminlettuce/0.0.6'},
 	{'name':'aa_airtable', 'url': 'http://pypi.python.org/pypi/aa_airtable/0.2'},
 	{'name':'aa_stripe', 'url': 'http://pypi.python.org/pypi/aa_stripe/0.4.1'},
 	{'name':'admin_bootstrap', 'url': 'http://pypi.python.org/pypi/admin_bootstrap/0.7.0'},
